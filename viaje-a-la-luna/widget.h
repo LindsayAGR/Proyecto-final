9b2566d0dd3a54f73ab83ddaeb2b5a7e791ebec9
LindsayG <lindsay.garcia@udea.edu.co>
2025-11-23 15:58:28 -0500

    Agrego incio al juego y boton Start

--- viaje-a-la-luna/widget.h
+++ viaje-a-la-luna/widget.h
@@ -6,6 +6,15 @@
 #include <QGraphicsPixmapItem>
 #include <QGraphicsView>
 #include "astronauta.h"
+#include <QTextBrowser>
+#include <QMediaPlayer>
+#include <QAudioOutput>
+#include <QTimer>
+#include <QPushButton>
+
+
+
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class Widget;
@@ -16,14 +25,29 @@ class Widget : public QWidget
 {
     Q_OBJECT
 
-public:
+public:    //funciones
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+    void mostrarInicio();
+    void iniciarIntroduccion();
+
+public slots: //sii conectar timer
+    void bgMove();
+
 private:
     Ui::Widget *ui;
-    QGraphicsScene *scene;
-    QGraphicsPixmapItem *bgImage;
-    astronauta *astronauta;
+    QGraphicsScene *scene1;
+    QGraphicsPixmapItem *bgImage1, *bgImage2;
+    astronauta *astro;
+    QGraphicsTextItem *texto;
+    QMediaPlayer *intro1, *introfon, *introinicio, *sonclick;
+    QTimer *bgTimer;
+
+    QGraphicsScene *sceneInicio;
+    QPushButton *btnStart;
+
+
+
 };
 #endif // WIDGET_H
